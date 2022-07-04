bb8647d0b5cb89eae4c2ef08272b27e3618200ef
Vladislav Skabeev <vladskab@mail.ru>
2022-07-04 23:48:22 +0400

    add new eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,11 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'max-len': ['error', {
+      'code': 300
+    }],
+    'vue/valid-v-for': 'off',
+    'vue/require-v-for-key': 'off',
+    'no-underscore-dangle': 'off'
   },
 };
